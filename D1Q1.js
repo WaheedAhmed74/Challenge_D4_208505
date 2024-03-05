302571ab0362386ec54d028760c0400159786d81
WaheedAhmed74 <159169508+WaheedAhmed74@users.noreply.github.com>
2024-03-05 19:15:37 +0500

    Add files via upload

---
+++ D1Q1.js
@@ -0,0 +1,52 @@
+// // Challenge : Day 1 (Question 1)
+// Install  Node.Js then Typescript and then VS Code
+// // Challenge : Day 1 (Question 2)
+// create a name in a variable
+// let name1: string = "Waheed"
+// console.log("Hellow Mr. "+name1+ " Would You like to learn some Typescript Today")
+// console.log("Hellow Mr."+ " " + name1 + " " + "Would You like to learn some Typescript Today")
+// // Challenge : Day 1 (Question 3)
+// create a name in a variable
+// let myname : string = "wAheEd aHmEd sOomRO"
+// console.log(" My Name in Orignal Text " + myname)
+// console.log(" My Name in LowerCase Text " + myname.toLowerCase())
+// console.log(" My Name in UpperCase Text " + myname.toUpperCase())
+// this Line learn from Internet but still no idea how it works (Title Case)
+// console.log("My Nam in TitleCase Text " + myname.toLowerCase().replace(/(?:^|\s)\w/g, match => match.toUpperCase()))
+// // Challenge : Day 2 (Question 1)
+// console.log(`"Albert Einstein once said, "A person who never made a mistake never tried anything new.""`)
+// // Challenge : Day 2 (Question 2)
+// let famous_person : string = "Albert Einstein";
+// let message : string = "A person who never made a mistake never tried anything new.";
+// console.log(`"${famous_person} once said, "${message}""`)
+// // Challenge : Day 2 (Question 3)
+// let stripping_name : string = "     Waheed   \t    \n"
+// console.log (stripping_name)
+// console.log (stripping_name.trim())
+// // Challenge : Day 3 (Question 1)
+// console.log(5+3)
+// console.log(9-1)
+// console.log(4*2)
+// console.log(16/2)
+// // Challenge : Day 3 (Question 2)
+// console.log(`5+3\n7+1\n4+4\n9-1`)
+// // Challenge : Day 3 (Question 3)
+// var favourite_number : number = 7;
+// console.log("Here is my favourite number " + favourite_number)
+// // Challenge : Day 4 (Question 1)
+// This program is written by Soomro Waheed Ahmed
+/* This is Day 4 Challenge Program
+    and Question 1 for commenting
+    these are multi lines Comments
+    This Challenge start on 05 March 2024
+*/
+// // Challenge : Day 4 (Question 2)
+// let names : string[] = ["Waheed", "Qamar", "Raza", "Maqsood", "Adnan"]
+// for (let i = 0; i < names.length; i ++){
+//     console.log(names[i])
+// }
+// // Challenge : Day 4 (Question 3)
+var names = ["Waheed", "Qamar", "Raza", "Maqsood", "Adnan"];
+for (var i = 0; i < names.length; i++) {
+    console.log("Assalam o Alaikum Dear " + names[i] + ", How are You?");
+}
